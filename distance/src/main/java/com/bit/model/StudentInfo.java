955999e0e0ba666be55d1b428d2959ec53456420
Nijat Aliyev <nijataliyev@paris-mbp.myfiosgateway.com>
2021-01-01 11:46:37 -0500

    additional fields

--- distance/src/main/java/com/bit/model/StudentInfo.java
+++ distance/src/main/java/com/bit/model/StudentInfo.java
@@ -1,8 +1,11 @@
 package com.bit.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 
 @Entity
@@ -14,19 +17,120 @@ public class StudentInfo {
     @GeneratedValue (strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
-
+    @NotNull(message = "First Name is mandatory")
+    @Size(min=2, max=30)
     private String fname;
+    @NotNull(message = "Last Name is mandatory")
+    @Size(min = 1, max = 30)
     private String lname;
+    @NotNull(message = "Address is mandatory")
+    @Size(min = 1, max = 120)
     private String address;
+    @NotNull(message = "City is mandatory")
+    @Size(min = 1, max = 30)
     private String city;
+    @NotNull(message = "State is mandatory")
+    @Size(min = 1, max = 30)
     private String state;
     private double distanceFromSchool;
+    @Size(min = 5, max = 10)
+    @NotNull(message = "Zip code is mandatory")
     private String zipCode;
+    @Size(min = 1, max = 2)
+    @NotNull(message = "Grade is mandatory")
+    private String grade;
+    @Size(min = 1, max = 50)
+    @NotNull(message = "School is mandatory")
+    private String school;
+    @Size(min = 1, max = 30)
+    @NotNull(message = "Parent Name is mandatory")
+    private String parentName;
+    @Size(min = 1, max = 30)
+    @NotNull(message = "Parent Name is mandatory")
+    private String parentEmailAddress;
+    @Size(min = 1, max = 15)
+    @NotNull(message = "Parent Name is mandatory")
+    private String parentPhoneNumber;
+    private String unit;
+    private boolean homeless;
+    private String mName;
 
+    private LocalDateTime formSubmitTime = LocalDateTime.now();
+
+    public LocalDateTime getFormSubmitTime() {
+        return formSubmitTime;
+    }
+
+    public void setFormSubmitTime(LocalDateTime formSubmitTime) {
+        this.formSubmitTime = formSubmitTime;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public String getParentEmailAddress() {
+        return parentEmailAddress;
+    }
+
+    public void setParentEmailAddress(String parentEmailAddress) {
+        this.parentEmailAddress = parentEmailAddress;
+    }
+
+    public String getParentPhoneNumber() {
+        return parentPhoneNumber;
+    }
+
+    public void setParentPhoneNumber(String parentPhoneNumber) {
+        this.parentPhoneNumber = parentPhoneNumber;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public boolean isHomeless() {
+        return homeless;
+    }
+
+    public void setHomeless(boolean homeless) {
+        this.homeless = homeless;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
 
     public StudentInfo() {
     }
-
     public String getZipCode() {
         return zipCode;
     }
