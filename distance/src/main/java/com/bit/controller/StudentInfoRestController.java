9eccbd2daaf4fd2bb1ad55fd78467cb3d9bde585
Nijat Aliyev <nijataliyev@paris-mbp.myfiosgateway.com>
2021-01-01 00:39:48 -0500

    cross

--- distance/src/main/java/com/bit/controller/StudentInfoRestController.java
+++ distance/src/main/java/com/bit/controller/StudentInfoRestController.java
@@ -7,7 +7,7 @@ import com.bit.repo.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class StudentInfoRestController {
 
