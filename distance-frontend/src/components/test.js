e40d151f77b9fc6321f5eeff9b1b33a8a870216b
Aziz Aghayev <naysaziz@gmail.com>
2021-01-03 01:40:40 -0500

    new pages and changes to front end

---
+++ distance-frontend/src/components/test.js
@@ -0,0 +1,22 @@
+const a = {
+  address: "39 Mass Ave",
+  city: "Harvard",
+  distanceFromSchool: 8.226756058215319,
+  enrollmentStatus: "free",
+  fname: "Test",
+  formSubmitTime: "2021-01-02T23:45:10.922",
+  grade: "1",
+  homeless: false,
+  id: "282b3121-f855-4aac-9cd8-f7050272fbbc",
+  lname: "Tester",
+  mName: "Tested",
+  parentEmailAddress: "aaghayev@psharvard.org",
+  parentName: "fsr",
+  parentPhoneNumber: "6175802949",
+  school: "RSS",
+  state: "MA",
+  unit: "wer",
+  zip: "01451",
+};
+
+print(a.zip);
