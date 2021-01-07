2a9bf03f4b928de04b20726bd03ba37018edda48
Nijat Aliyev <nijataliyev@Paris-MacBook-Pro.local>
2021-01-07 03:34:35 -0500

    payment stripe initial

---
+++ distance/src/main/java/com/bit/services/StripeClient.java
@@ -0,0 +1,45 @@
+package com.bit.services;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.model.Customer;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.PaymentMethod;
+import com.stripe.param.PaymentIntentCreateParams;
+import com.stripe.param.PaymentMethodAttachParams;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class StripeClient {
+
+    @Autowired
+    StripeClient() {
+        Stripe.apiKey = "sk_test_51I5QOxJuvhMix0vIOQyOOupcFxWcybrKZmJ1ndjkKlJg0gy7ZinyIlK7zPl1HAYhv9mbGSXdUS48ZkvldCuDR7po00nSxeHBwq";
+    }
+
+    public Map<String, Object> chargeCreditCard(String token, int amount) throws Exception {
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", amount);
+        params.put("currency", "usd");
+        params.put("payment_method", token);
+        params.put("confirm", true);
+        PaymentIntent intent = PaymentIntent.create(params);
+        return responseMap(intent);
+    }
+
+    private Map<String, Object> responseMap(PaymentIntent paymentIntent){
+        Map<String, Object> response = new HashMap<String, Object>();
+        String strId = paymentIntent.getId();
+        response.put("seller_message",paymentIntent.getCharges().getData().get(0).getOutcome().getSellerMessage());
+        response.put("confirmation",strId.substring(strId.length()-3).toUpperCase()+paymentIntent.getCharges().getData().get(0).getCreated());
+        response.put("status", paymentIntent.getStatus());
+        return response;
+    }
+}
