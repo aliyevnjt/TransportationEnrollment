7a41ece392c85be20e23ca388ae93123960e42f6
Aziz <naysaziz@gmail.com>
2021-05-10 14:56:05 -0400

    Mobile friendly changes

--- distance-frontend/src/components/toolbox/Dropdown.jsx
+++ distance-frontend/src/components/toolbox/Dropdown.jsx
@@ -3,41 +3,34 @@ import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
 
 const Dropdown = (props) => {
-  const {
-    id, options, onChange, value, required, defaultVal
-  } = props;
+  const { id, options, onChange, value, required, defaultVal } = props;
   let { label } = props;
   // console.log('getRegistrationYear:', value);
   // console.log('DROPDOWN component is loaded', value);
   // FIXME look into defaultValue in options
   return (
-    <Form.Group>
-      <Form.Label>
-        {label}
-      </Form.Label>
-      <Form.Control
-        as="select"
-        id={id}
-        required={required}
-        onChange={onChange}
-      >
+    <Form.Group lg="3" md="4" sm="6" xs="12">
+      <Form.Label>{label}</Form.Label>
+      <Form.Control as="select" id={id} required={required} onChange={onChange}>
         <option value="">{defaultVal}</option>
-        {
-          options.map((o) => (
-            <option key={o.label} defaultValue={value === o.label} value={o.value}>{o.label}</option>
-          ))
-        }
+        {options.map((o) => (
+          <option
+            key={o.label}
+            defaultValue={value === o.label}
+            value={o.value}>
+            {o.label}
+          </option>
+        ))}
       </Form.Control>
       <Form.Control.Feedback type="invalid">
-            Please choose a value.
+        Please choose a value.
       </Form.Control.Feedback>
     </Form.Group>
   );
 };
 
 Dropdown.defaultProps = {
-  required: true
-
+  required: true,
 };
 Dropdown.propTypes = {
   id: PropTypes.string.isRequired,
@@ -46,6 +39,6 @@ Dropdown.propTypes = {
   onChange: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
   required: PropTypes.bool,
-  defaultVal: PropTypes.string
+  defaultVal: PropTypes.string,
 };
 export default Dropdown;
